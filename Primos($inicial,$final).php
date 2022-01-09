ee54c33e903a7f4ee796130256c3a255066b781d
Welington Carlos Alves de Almeida Filho <38169533+WelingtonCarlos@users.noreply.github.com>
2022-01-09 14:29:12 -0300

    Update Primos($inicial,$final).php

--- Primos($inicial,$final).php
+++ Primos($inicial,$final).php
@@ -1,37 +1,72 @@
 <?php
 
 
-Primos(rand(1,50), rand(51,100));
+Primos(rand(1,9999), rand(1,9999));
 
-function Primos($inicial, $final)
-{
+function Primos($inicial, $final) {
+
     $numeroPrimo = array();
 
     // o incremento e decremetno abaixo é para encontrar os números Primo ENTRE os valores inicial e final
     // portanto, se faz necessário o incremento no inicial para pegar o valor logo a cima do inicial e decremento para pegar o valor logo abaixo do final
 
-    $inicial++;
-    $final--;
+    
 
-    for ($i = $inicial; $i <= $final; $i++) {
-        $cont = 0;
-        for ($j = 1; $j <= $i; $j++) {
-            if ($i % $j == 0) { // é realizada a divisão entre o valor de i e todos os números seguintes até chegar no valor de $final
-                $cont++;
-            }
-        }
-        if ($cont == 2) {
-            $numeroPrimo[] = $i;
-        }
-    }
+    if ($inicial < $final) {
 
-    // e no abaixo, invertemos para pegarmos o valores originais dos números inicial e final.
 
-    $inicial--;
-    $final++;
+    	$inicial++;
+    	$final--;
 
-    echo "Número Inicial: ".$inicial."<br>";
-    echo "Número Final: ".$final."<br>"."<br>";
+	    for ($i = $inicial; $i <= $final; $i++) {
+	        $cont = 0;
+	        for ($j = 1; $j <= $i; $j++) {
+	            if ($i % $j == 0) {
+	                $cont++;
+	            }
+	        }
+	        if ($cont == 2) {
+	            $numeroPrimo[] = $i;
+	        }
+	    }
 
-    echo "Encontrados ".count($numeroPrimo)." números numeroPrimo, são eles: ".implode(", ", $numeroPrimo)."<br>";
+	    // e no abaixo, invertemos para pegarmos o valores originais dos números inicial e final.
+
+	    $inicial--;
+	    $final++;
+
+	    echo "Número Inicial: ".$inicial."<br>";
+	    echo "Número Final: ".$final."<br>"."<br>";
+
+	    echo "Encontrados ".count($numeroPrimo)." números numeroPrimo, são eles: ".implode(", ", $numeroPrimo)."<br>";
+	}
+
+	elseif ($inicial > $final) {
+
+		$inicial--;
+    	$final++;
+		
+		for ($i = $final; $i <= $inicial; $i++) {
+	        $cont = 0;
+	        for ($j = 1; $j <= $i; $j++) {
+	            if ($i % $j == 0) {
+	                $cont++;
+	            }
+	        }
+	        if ($cont == 2) {
+	            $numeroPrimo[] = $i;
+	        }
+	    }
+
+	    // e no abaixo, invertemos para pegarmos o valores originais dos números inicial e final.
+
+	    $inicial++;
+	    $final--;
+
+	    echo "Número Inicial: ".$final."<br>";
+	    echo "Número Final: ".$inicial."<br>"."<br>";
+
+	    echo "Encontrados ".count($numeroPrimo)." números numeroPrimo, são eles: ".implode(", ", $numeroPrimo)."<br>";
+
+	}
 }
